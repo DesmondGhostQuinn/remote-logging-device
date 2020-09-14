d60972166f7344c465602244fcedc8735c698897
Desmond Quinn <DJ_quinn74@yahoo.com>
2020-09-14 07:18:55 -0700

    Update client.py

--- client.py
+++ client.py
@@ -6,17 +6,10 @@ from datetime import datetime
 
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-#HOST="192.168.0.101"
 PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    #str=''
-    """"arga = sys.argv[1]
-    argb = sys.argv[2]
-    argc = sys.argv[3]
-    argd = sys.argv[4]
-    str=arga+argb+argc+argd"""
     str=input("enter the code")
     s.send(str.encode())
     t=datetime.now()
@@ -27,10 +20,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     t2=datetime.now()
     str3=t2.strftime('%x/%X')
     s.send(str3.encode())
-    """""duration=t2-t
-    duration=duration.strftime('%x/%X')
-    s.send(duration.encode())"""
-    #s.sendall(message.encode("hello world"))
     data = s.recv(1024)
 
 
