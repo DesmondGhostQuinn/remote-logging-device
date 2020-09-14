133d60e020c0e242a0cba47398b251f2d5775336
Desmond Quinn <DJ_quinn74@yahoo.com>
2020-09-14 07:16:34 -0700

    Update web2.py

--- webapp/web2.py
+++ webapp/web2.py
@@ -1,34 +1,22 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
 from flask_pymongo import MongoClient
-#from flask.ext.pymongo import PyMongo
 import smtplib
 import yagmail
 
 
 app = Flask(__name__)
 
+#specify the mongo database being used
 app.config['MONGO_DBNAME'] = 'thedb'
-#app.config['MONGO_URI'] = MongoClient('mongodb://127.0.0.1:27017/') #added recent
 app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/thedb"
-#app.url_map.strict_slashes = False
-#client = MongoClient('MONGO_URI')
-#db = client.thedb
-#collection = db['gamerz']
-#collection = db.gamerz
-""""client = MongoClient()
-db = client.thedb
-collection = db.gamerz"""
 
 
 mongo = PyMongo(app)
 
 @app.route('/', methods=['GET'])
-
-
-
 def home():
-    return render_template('ehome.html')      #just added
+    return render_template('ehome.html')      
 
 @app.route('/tnc')
 def tnc():
@@ -58,35 +46,6 @@ def member():
 def gallery():
     return render_template('gallery.html')
 
-
-@app.route('/profile', methods=["POST", "GET"])
-def profile():
-	if 'tag' in session:
-		p = mongo.db.gamer.find_one({'tag': session['tag']})
-		return render_template('eprofile.html',username=p['name'],points=p['points'])		#just added
-
-	return render_template('homepage.html')
-
-
-@app.route('/login', methods=["POST", "GET"])
-def login():
-    if request.method == 'POST':
-		#users = mongo.db.gamer
-        t = mongo.db.gamer.find_one({'tag' : request.form['tag']})
-        if t:
-            session['tag'] = request.form['tag']
-            session['logged_in'] = True
-            return redirect(url_for('profile'))
-
-        return 'Invalid Gamertag'
-    return render_template('elogin.html')
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    #flash('You were logged out.')
-    return redirect(url_for('home'))
-
 @app.route('/faq')
 def faq():
     return render_template('efaq.html')
@@ -95,13 +54,37 @@ def faq():
 def corporate():
     return render_template('corporate.html')
 
+@app.route('/profile', methods=["POST", "GET"])
+def profile():
+	if 'tag' in session:
+		p = mongo.db.gamer.find_one({'tag': session['tag']})
+		return render_template('eprofile.html',username=p['name'],points=p['points'])		
+	return render_template('homepage.html')
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        t = mongo.db.gamer.find_one({'tag' : request.form['tag']})
+        if t:
+            session['tag'] = request.form['tag']
+            session['logged_in'] = True
+            return redirect(url_for('profile'))
+	return 'Invalid Gamertag'
+    return render_template('elogin.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('home'))
+
 @app.route('/contact', methods=["POST", "GET"])
 def contact():
     if request.method == 'POST':
         reply_to = request.form['email']
         message = request.form['message']
         message = reply_to+'--says that--'+message
-        yag = yagmail.SMTP('endsemproject24@gmail.com', 'sainasimran1')	#to initialize
+        yag = yagmail.SMTP('endsemproject24@gmail.com', 'sainasimran1')	#the email id and password is specified
         yagmail.SMTP('endsemproject24@gmail.com').send('endsemprojectproject24@gmail.com', reply_to, message)
         return redirect(url_for('home'))
     return render_template('contact.html')
@@ -109,21 +92,16 @@ def contact():
 @app.route('/register', methods=["POST", "GET"])
 def register():
     if request.method == 'POST' :
-
-        m1=mongo.db.gamer.find_one({'title': "forc"})
+        m1=mongo.db.gamer.find_one({'title': "forc"})	#document with this particular field keeps track of the count
         if m1:
             n=m1['count']
         n1=int(n)
         nx=str(n1+1)
-        mongo.db.gamer.update(m1,{'$set':{'count':n1+1}})
-        mongo.db.gamer.insert({'name' : request.form['name'], 'email' : request.form['email'], 'phone' : request.form['phone'], 'tag':nx, 'points': 0, 'index' : 0, 'age': request.form['age']})
-        #in=mongo.db.gamerz.find().sort({age:-1}).limit(1)
-        #m1=mongo.db.gamer.find().sort( 'index' , -1 )
-
-
-        return render_template('gametagdisp.html', id = nx)
-        #return('user registered with gamertag %s' %nx)
-
+	#count is updated
+        mongo.db.gamer.update(m1,{'$set':{'count':n1+1}})	
+        #collection is updated
+	mongo.db.gamer.insert({'name' : request.form['name'], 'email' : request.form['email'], 'phone' : request.form['phone'], 'tag':nx, 'points': 0, 'index' : 0, 'age': request.form['age']})
+ 	return render_template('gametagdisp.html', id = nx)	#id of the new user is returned
     return render_template('esignup.html')
 
 
